3f381af463d7ff3e4b7731b91c07a2a452af61dd
xxbiohazrdxx <xxbiohazrdxx@users.noreply.github.com>
2023-09-01 16:02:34 -0400

    Reduce lifespan of dbcontext in metadata parsing, fix bundled update logic

--- UpdateProcessor/UpdateProcessorFunction.cs
+++ UpdateProcessor/UpdateProcessorFunction.cs
@@ -71,10 +71,16 @@ namespace UpdateProcessor
 		{
 			logMessagePrefix = "Loading classification metadata";
 
-			using var dbContext = await dbContextFactory.CreateDbContextAsync(Token);
-			var allExistingCategories = await dbContext.Categories.ToListAsync(Token);
-			var allExistingProducts = await dbContext.Products.ToListAsync(Token);
-			var allExistingDetectoids = await dbContext.Detectoids.ToListAsync(Token);
+			List<Product> allExistingProducts;
+			List<Category> allExistingCategories;
+			List<Detectoid> allExistingDetectoids;
+
+			using (var dbContext = await dbContextFactory.CreateDbContextAsync(Token))
+			{
+				allExistingProducts = await dbContext.Products.ToListAsync(Token);
+				allExistingCategories = await dbContext.Categories.ToListAsync(Token);
+				allExistingDetectoids = await dbContext.Detectoids.ToListAsync(Token);
+			}
 
 			UpstreamCategoriesSource categoriesSource = new(Endpoint.Default);
 			categoriesSource.MetadataCopyProgress += CopyProgress;
@@ -96,58 +102,60 @@ namespace UpdateProcessor
 				allExistingProducts.Count,
 				allExistingDetectoids.Count);
 
-
 			// Iterate through all of the metadata returned by the upstream source
 			await foreach (var currentClassification in allClassificationCategories)
 			{
-				// Depending on the type of the MicrosoftUpdatePackage, create the entity type and insert into the correct container
-				if (currentClassification is ClassificationCategory)
+				using (var dbContext = await dbContextFactory.CreateDbContextAsync(Token))
 				{
-					await dbContext.Categories.AddAsync(new Category()
+					// Depending on the type of the MicrosoftUpdatePackage, create the entity type and insert into the correct container
+					if (currentClassification is ClassificationCategory)
 					{
-						Id = currentClassification.Id.ID,
-						Name = currentClassification.Title,
-						Enabled = configuration.EnabledCategories.Contains(currentClassification.Id.ID)
-					}, Token);;
+						await dbContext.Categories.AddAsync(new Category()
+						{
+							Id = currentClassification.Id.ID,
+							Name = currentClassification.Title,
+							Enabled = configuration.EnabledCategories.Contains(currentClassification.Id.ID)
+						}, Token); ;
 
-					Status.CategoryCount++;
-				}
-				else if (currentClassification is ProductCategory)
-				{
-					await dbContext.Products.AddAsync(new Product()
+						Status.CategoryCount++;
+					}
+					else if (currentClassification is ProductCategory)
 					{
-						Id = currentClassification.Id.ID,
-						Revision = currentClassification.Id.Revision,
-						Name = currentClassification.Title,
-						Enabled = configuration.EnabledProducts.Contains(currentClassification.Id.ID),
-						Categories = currentClassification.Categories?.Select(y => y.ToString()).ToList() ?? new List<string>()
-					}, Token);
+						await dbContext.Products.AddAsync(new Product()
+						{
+							Id = currentClassification.Id.ID,
+							Revision = currentClassification.Id.Revision,
+							Name = currentClassification.Title,
+							Enabled = configuration.EnabledProducts.Contains(currentClassification.Id.ID),
+							Categories = currentClassification.Categories?.Select(y => y.ToString()).ToList() ?? new List<string>()
+						}, Token);
 
-					Status.ProductCount++;
-				}
-				// While we don't actually use detectoids, storing them in the database reduces delta metadata sync time
-				else if (currentClassification is DetectoidCategory)
-				{
-					await dbContext.Detectoids.AddAsync(new Detectoid()
+						Status.ProductCount++;
+					}
+					// While we don't actually use detectoids, storing them in the database reduces delta metadata sync time
+					else if (currentClassification is DetectoidCategory)
 					{
-						Id = currentClassification.Id.ID,
-						Revision = currentClassification.Id.Revision,
-						Name = currentClassification.Title
-					}, Token);
+						await dbContext.Detectoids.AddAsync(new Detectoid()
+						{
+							Id = currentClassification.Id.ID,
+							Revision = currentClassification.Id.Revision,
+							Name = currentClassification.Title
+						}, Token);
 
-					Status.DetectoidCount++;
+						Status.DetectoidCount++;
+					}
+					else
+					{
+						break;
+					}
+
+					await dbContext.SaveChangesAsync(Token);
+
+					logger.LogTrace("Added classification: {Id}:{Revision} - {Title}",
+						currentClassification.Id.ID,
+						currentClassification.Id.Revision,
+						currentClassification.Title);
 				}
-				else
-				{
-					break;
-				}
-
-				await dbContext.SaveChangesAsync(Token);
-
-				logger.LogTrace("Added classification: {Id}:{Revision} - {Title}",
-					currentClassification.Id.ID,
-					currentClassification.Id.Revision,
-					currentClassification.Title);
 			}
 		}
 
@@ -156,10 +164,16 @@ namespace UpdateProcessor
 		{
 			logMessagePrefix = "Loading update metadata";
 
-			using var dbContext = await dbContextFactory.CreateDbContextAsync(Token);
-			var allExistingUpdates = await dbContext.Updates.ToListAsync(Token);
-			var allProducts = await dbContext.Products.ToListAsync(Token);
-			var allCategories = await dbContext.Categories.ToListAsync(Token);
+			List<Update> allExistingUpdates;
+			List<Product> allProducts;
+			List<Category> allCategories;
+
+			using (var dbContext = await dbContextFactory.CreateDbContextAsync(Token))
+			{
+				allExistingUpdates = await dbContext.Updates.ToListAsync(Token);
+				allProducts = await dbContext.Products.ToListAsync(Token);
+				allCategories = await dbContext.Categories.ToListAsync(Token);
+			}
 
 			// Filter so only the particular types and products wanted will be downloaded and processed
 			var updatesFilter = new UpstreamSourceFilter();
@@ -237,8 +251,11 @@ namespace UpdateProcessor
 
 				Status.UpdateCount++;
 
-				await dbContext.Updates.AddAsync(newUpdate, Token);
-				await dbContext.SaveChangesAsync(Token);
+				using (var dbContext = await dbContextFactory.CreateDbContextAsync(Token))
+				{
+					await dbContext.Updates.AddAsync(newUpdate, Token);
+					await dbContext.SaveChangesAsync(Token);
+				}
 
 				logger.LogTrace("Added update: {Id}:{Revision} - {Title}",
 					currentUpdate.Id.ID,
@@ -250,9 +267,13 @@ namespace UpdateProcessor
 			logger.LogInformation("Processing bundled update file lists");
 
 			// Iterate through all updates that have bundled updates, copying the files from the bundled update to the primary update entity
-			var allBundledUpdates = await dbContext.Updates
-				.Where(x => x.BundledUpdates != null)
+			List<Update> allBundledUpdates;
+			using (var dbContext = await dbContextFactory.CreateDbContextAsync(Token))
+			{
+				allBundledUpdates = await dbContext.Updates
+				.FromSqlRaw("SELECT * FROM x WHERE ARRAY_LENGTH(x.BundledUpdates) > 0")
 				.ToListAsync(Token);
+			}
 
 			int bundleProgress = 1;
 			foreach (var currentBundle in allBundledUpdates)
@@ -260,36 +281,37 @@ namespace UpdateProcessor
 				Status.AddLogMessage(string.Format("Processing bundled update file lists: {0}/{1}", bundleProgress, allBundledUpdates.Count));
 				logger.LogInformation("Processing bundled update file lists: {Current}/{Total}", bundleProgress, allBundledUpdates.Count);
 
-				var update = await dbContext.Updates
-					.Where(x => x.Id == currentBundle.Id)
-					.SingleAsync(Token);
+				using (var dbContext = await dbContextFactory.CreateDbContextAsync(Token))
+				{
+					var update = await dbContext.Updates
+						.Where(x => x.Id == currentBundle.Id)
+						.SingleAsync(Token);
 
-				var bundledUpdateFiles = await dbContext.Updates
-					.FromSqlRaw("SELECT * FROM x WHERE ARRAY_CONTAINS({0}, x.id)", update.BundledUpdates)
-					//.Where(x => currentBundle.BundledUpdates.Any(y => y == x.Id.ToString()))
-					.ToListAsync(Token);
+					var bundledUpdateFiles = await dbContext.Updates
+						.FromSqlRaw("SELECT * FROM x WHERE ARRAY_CONTAINS({0}, x.id)", update.BundledUpdates)
+						.ToListAsync(Token);
 
-				update.Files = bundledUpdateFiles
-					.SelectMany(x => x.Files)
-					.Select(x => new File()
-					{
-						FileName = x.FileName,
-						Source = x.Source,
-						ModifiedDate = x.ModifiedDate,
-						Digest = new FileDigest()
+					update.BundledUpdates = new List<string>();
+					update.Files = bundledUpdateFiles
+						.SelectMany(x => x.Files)
+						.Select(x => new File()
 						{
-							Algorithm = x.Digest.Algorithm,
-							Value = x.Digest.Value
-						},
-						Size = x.Size
-					})
-					.ToList();
+							FileName = x.FileName,
+							Source = x.Source,
+							ModifiedDate = x.ModifiedDate,
+							Digest = new FileDigest()
+							{
+								Algorithm = x.Digest.Algorithm,
+								Value = x.Digest.Value
+							},
+							Size = x.Size
+						})
+						.ToList();
+					
+					await dbContext.SaveChangesAsync(Token);
+					logger.LogTrace("Added bundled files: {Id} - {Title}", update.Id, update.Title);
+				}
 
-				update.BundledUpdates = new List<string>();
-
-				await dbContext.SaveChangesAsync(Token);
-
-				logger.LogTrace("Added bundled files: {Id} - {Title}", update.Id, update.Title);
 				bundleProgress++;
 			}
 		}
