cfdf818b2716ba14c61b71b0f99ab4ed6be079b4
xxbiohazrdxx <xxbiohazrdxx@users.noreply.github.com>
2023-09-03 20:52:03 -0400

    Hide Enabled property from Category controller

--- UpdateLib/Models/Category.cs
+++ UpdateLib/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿namespace UpdateLib.Models
+﻿using System.Text.Json.Serialization;
+
+namespace UpdateLib.Models
 {
 	// This additional OwnedCategory class is needed as it is an owned type of the Update entity. Category cannot be used since it is 
 	// already mapped to a container. 
@@ -14,6 +16,7 @@
 	{
 		public Guid Id { get; set; } = default!;
 		public string Name { get; set; } = default!;
+		[JsonIgnore]
 		public bool Enabled { get; set; } = default!;
 
 		public Category() { }
